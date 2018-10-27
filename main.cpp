92bcc86b6f86bea042e2d0eabae7d8caf4e3bc14
Andrea Nobile <and.nobile@gmail.com>
2018-10-27 16:34:57 +0200

    add delay in loop to fetch frames at streaming frame rate

--- main.cpp
+++ main.cpp
@@ -4,10 +4,74 @@
 #include <cstdlib>
 #include <unistd.h>
 
+#include <chrono>
+
 
 #include "streamer/streamer.hpp"
 
 using namespace streamer;
+
+
+class MovingAverage
+{
+    int size;
+    int pos;
+    bool crossed;
+    std::vector<double> v;
+
+public:
+    explicit MovingAverage(int sz)
+    {
+        size = sz;
+        v.resize(size);
+        pos = 0;
+        crossed = false;
+    }
+
+    void add_value(double value)
+    {
+        v[pos] = value;
+        pos++;
+        if(pos == size) {
+            pos = 0;
+            crossed = true;
+        }
+    }
+
+    double get_average()
+    {
+        double avg = 0.0;
+        int last = crossed ? size : pos;
+        int k=0;
+        for(k=0;k<last;k++) {
+            avg += v[k];
+        }
+        return avg / (double)last;
+    }
+};
+
+
+static void add_delay(size_t streamed_frames, size_t fps, double elapsed, double avg_frame_time)
+{
+    //compute min number of frames that should have been streamed based on fps and elapsed
+    double dfps = fps;
+    size_t min_streamed = (size_t) (dfps*elapsed);
+    size_t min_plus_margin = min_streamed + 20;
+
+    if(streamed_frames > min_plus_margin) {
+        size_t excess = streamed_frames - min_plus_margin;
+        double dexcess = excess;
+
+        //add a delay ~ excess*processing_time
+
+#ifdef SHOW_DELAY
+        double delay = dexcess*avg_frame_time*1000000.0;
+        printf("frame %07lu adding delay %.4f\n", streamed_frames, delay);
+        printf("avg fps = %.2f\n", streamed_frames/elapsed);
+#endif
+        usleep(dexcess*avg_frame_time*1000000.0);
+    }
+}
 
 
 int main(int argc, char *argv[])
@@ -32,20 +96,41 @@ int main(int argc, char *argv[])
     int cap_fps = video_capture.get(cv::CAP_PROP_FPS);
     printf("video info w = %d, h = %d, fps = %d\n", cap_frame_width, cap_frame_height, cap_fps);
 
+    int stream_fps = cap_fps;
     int bitrate = 500000;
     Streamer streamer;
     StreamerConfig streamer_config(cap_frame_width, cap_frame_height,
-                                   320, 180,
-                                   cap_fps, bitrate, "high444", "rtmp://localhost/live/mystream");
+                                   640, 360,
+                                   stream_fps, bitrate, "high444", "rtmp://localhost/live/mystream");
 
     streamer.enable_av_debug_log();
     streamer.init(streamer_config);
 
     cv::Mat read_frame;
     bool ok = video_capture.read(read_frame);
+    size_t streamed_frames = 0;
+
+    std::chrono::high_resolution_clock clk;
+    std::chrono::high_resolution_clock::time_point time_start = clk.now();
+    std::chrono::high_resolution_clock::time_point time_stop = time_start;
+    std::chrono::high_resolution_clock::time_point time_prev = time_start;
+
+    MovingAverage moving_average(10);
+    double avg_frame_time;
+
     while(ok) {
         streamer.stream_frame(read_frame);
+
+        streamed_frames++;
+        time_stop = clk.now();
+        std::chrono::duration<double> elapsed_time = std::chrono::duration_cast<std::chrono::duration<double>>(time_stop - time_start);
+        std::chrono::duration<double> frame_time = std::chrono::duration_cast<std::chrono::duration<double>>(time_stop - time_prev);
+        moving_average.add_value(frame_time.count());
+        avg_frame_time = moving_average.get_average();
+        add_delay(streamed_frames, stream_fps, elapsed_time.count(), avg_frame_time);
+
         ok = video_capture.read(read_frame);
+        time_prev = time_stop;
     }
     video_capture.release();
 
